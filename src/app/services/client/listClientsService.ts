0e7a823f387834da8d9329e230e8236ea7f54564
Jean Carlos <lopesjean81@gmail.com>
2025-05-24 10:43:41 -0300

    feat(redis): caching lis client requests

--- src/app/services/client/listClientsService.ts
+++ src/app/services/client/listClientsService.ts
@@ -1,10 +1,21 @@
 import { Client } from "../../../domain/entities/Client";
+import { IRedisRepository } from "../../../domain/repositories/redisRepository";
 import { IBaseRepository } from "../../../shared/base/baseRepository";
 
 export class ListClientsByIdService {
-  constructor(private readonly clientRepository: IBaseRepository<Client>) {}
+  constructor(
+    private readonly clientRepository: IBaseRepository<Client>,
+    private readonly redisClient: IRedisRepository
+  ) {}
 
   async execute(): Promise<Client[]> {
-    return await this.clientRepository.findAll();
+    const cachedKey = "clients:all";
+    const cachedClients = await this.redisClient.get(cachedKey);
+    if (cachedClients) return JSON.parse(cachedClients);
+
+    const clients = await this.clientRepository.findAll();
+    this.redisClient.set(cachedKey, JSON.stringify(clients), 60 * 5);
+
+    return clients;
   }
 }
