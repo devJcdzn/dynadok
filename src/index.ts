387559a68934524880edbf4df046dd783e5dff97
Jean Carlos <lopesjean81@gmail.com>
2025-05-25 01:25:21 -0300

    feat(messaging): instance messaging workers on server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { clientRoutes } from "./interfaces/routes/clientRoutes";
 import { connectToDatabase } from "./infra/db/mongodb/connection";
 import { config } from "dotenv";
+import { ClientEventsConsumer } from "./infra/messaging/consumers/clientEventsConsumer";
+import { EventBus } from "./infra/messaging/eventBus";
 
 config();
 
@@ -11,12 +13,16 @@ app.use(express.json());
 app.use(clientRoutes);
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/clients";
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
+const eventBus = new EventBus();
 
 async function startServer() {
   try {
     await connectToDatabase();
+
+    const consumer = new ClientEventsConsumer(eventBus);
+    await consumer.start();
+
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
