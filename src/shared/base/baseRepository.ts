36d241d951a98be89fda03e291aeb14d18b9a9a3
Jean Carlos <lopesjean81@gmail.com>
2025-05-25 11:14:52 -0300

    feat(shared/base): add delete method

--- src/shared/base/baseRepository.ts
+++ src/shared/base/baseRepository.ts
@@ -3,4 +3,5 @@ export interface IBaseRepository<T> {
   update(id: string, item: Partial<T>): Promise<T>;
   findById(id: string): Promise<T>;
   findAll(): Promise<T[]>;
+  delete(id: string): Promise<string>;
 }
